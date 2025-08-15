04b7e44496f3cde89004937303d4f223bb054fd2
Mamta Jangra <mamtajangra204@gmail.com>
2025-08-15 21:27:12 +0530

    Refactor sendPhoto function to use threading for sending photos to multiple chat IDs

--- send_picture.py
+++ send_picture.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import threading
 import dotenv
 
 
@@ -7,16 +8,25 @@ dotenv.load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 pic_path = r'files\files\IXKP8281.JPG'
-chat_ids = [6736740776,5412578346]  
+chat_ids = [6736740776,4947220003]
 
 def sendPhoto(chat_ids , pic_path):
-    for chat_id in chat_ids:
-        url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"  
-        with open(pic_path, 'rb') as photo:
-            files = {"photo": photo}
-            data = {"chat_id": chat_id}
-            response = requests.post(url, data=data, files=files)
-            print(response.json()) 
+    with open(pic_path, 'rb') as photo:
+        files = {"photo": photo}
+        data = {"chat_id": chat_id}
+        response = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendPhoto",data = data,files = files )
+        print(response.json()) 
 
             
-sendPhoto(chat_ids , pic_path)            
+  
+list_of_threads = []
+
+for chat_id in chat_ids:
+    for i in range(2):
+        g = threading.Thread(target=sendPhoto, args=(chat_id, pic_path))
+        list_of_threads.append(g)
+        g.start()
+
+
+for thread in list_of_threads:
+    thread.join()         
