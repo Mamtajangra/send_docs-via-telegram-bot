9fb277879f846d3ac9c08f220aeec590f2ea4ca8
Mamta Jangra <mamtajangra204@gmail.com>
2025-10-25 16:25:48 +0530

    updated audio section

--- send_audio.py
+++ send_audio.py
@@ -6,8 +6,8 @@ import dotenv
 dotenv.load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-audio_path = r'files\files\audio.mp3'
-chat_ids = [6736740776,5412578346]  
+audio_path = r'files\file\vc.mp3'
+chat_ids = [5412578346]  
 
 def sendAudio(chat_ids , audio_path):
     for chat_id in chat_ids:
