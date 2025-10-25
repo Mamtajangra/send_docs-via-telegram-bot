42bbcb5b97ccadb318a64153142c8d7e214573d4
Mamta Jangra <mamtajangra204@gmail.com>
2025-10-25 16:26:54 +0530

    send video to someone via bot

--- send_video.py
+++ send_video.py
@@ -3,20 +3,20 @@ import requests
 import dotenv
 
 
+
 dotenv.load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-video_path = r'files\files\Banda Ban Ja - Garry Sandhu.mp4'
-chat_ids = [6736740776,5412578346]  
+vid_path = r'files/file/Banda Ban Ja - Garry Sandhu.mp4'
+chat_id = 5211817036
 
-def sendVideo(chat_ids , video_path):
-    for chat_id in chat_ids:
-        url = f"https://api.telegram.org/bot{TOKEN}/sendVideo"  
-        with open(video_path, 'rb') as video:
-            files = {"video": video}
-            data = {"chat_id": chat_id}
-            response = requests.post(url, data=data, files=files)
-            print(response.json()) 
+def sendVideo(chat_id, vid_path):
+    with open(vid_path, 'rb') as video:
+        files = {"video": video}
+        data = {"chat_id": chat_id}
+        response = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendVideo",data = data,files = files )
+        print(response.json())
+
+sendVideo(chat_id, vid_path)         
 
             
-sendVideo(chat_ids , video_path)            
